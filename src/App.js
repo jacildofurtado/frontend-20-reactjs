220d2e87ac91526cb38f52cc9b9f7154bba45085
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2022-11-08 11:14:35 -0300

    Update code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import './App.css';
-import Header from './Header';
+import React, { useState, useEffect } from 'react';
 
-function App(props) {
+import Api from './Api';
+
+
+
+export default function App() {
+
+  
+
+  
+
+  const [nomes, setNomes] = useState([{}]);
+
+
+
+  async function initApi(){
+
+      
+
+      let pegaNomes = await Api.getPersons();
+
+      setNomes(pegaNomes);
+
+  }
+
+  
+
+
+
+  useEffect(() => {
+
+
+
+      initApi();
+
+      
+
+  },[]);
+
+  
+
+  
+
   return (
-    <div className="App">
 
-      <Header />
+    <div>
+
+      {
+
+        nomes.map(function(data){
+
+          return(
+
+            <div>
+
+              <h2>{data.name} | {data.email}</h2>
+
+            </div>
+
+          )
+
+        })
+
+      }
+
     </div>
-  );
-}
 
-export default App;
+  );
+
+   
+
+}
